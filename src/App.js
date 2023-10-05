d1e3930a29ec442b97cfe8c3e3c87fdfeb7fb04f
Tetiana <butkaluk22@gmail.com>
2023-10-05 18:56:20 +0200

    add onSubmit in form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import Costs from "./components/Costs/Costs";
+import NewCost from "./components/NewCost/NewCost";
 import "./index.css";
 
 const cost = [
-  { date: new Date(2023, 9, 30), description: "Airpods", amount: 159.99 },
-  { date: new Date(2023, 8, 4), description: "MacBook", amount: 4299.99 },
-  { date: new Date(2023, 7, 18), description: "Iphone", amount: 999.99 },
+  {
+    date: new Date(2023, 9, 30),
+    description: "Airpods",
+    amount: 159.99,
+    id: "c1",
+  },
+  {
+    date: new Date(2023, 8, 4),
+    description: "MacBook",
+    amount: 4299.99,
+    id: "c2",
+  },
+  {
+    date: new Date(2023, 7, 18),
+    description: "Iphone",
+    amount: 999.99,
+    id: "c3",
+  },
 ];
 
 function App() {
+  const addCostHadler = (cost) => {
+    console.log(cost);
+    console.log("App component");
+  };
   return (
     <div className="App">
       <h1 className="title">Expense-management</h1>
-      <Costs cost={cost}></Costs>;
+      <NewCost onAddCost={addCostHadler} />
+      <Costs cost={cost} />;
     </div>
   );
 }
---
+++ src/components/NewCost/CostForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./CostForm.css";
+
+const CostForm = (props) => {
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  // const [userInput, setUserInput] = useState({
+  //   name: "",
+  //   amount: "",
+  //   date: "",
+  // });
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   name: event.target.value,
+    // });
+  };
+  const amountChangeHandler = (event) => {
+    setInputAmount(event.target.value);
+    // setUserInput((previosState) => return {
+    //   ...previosState,
+    //   amount: event.target.value,
+    // });
+  };
+  const dateChangeHandler = (event) => {
+    setInputDate(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costDate = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+    props.onSaveCostData(costDate);
+    setInputName("");
+    setInputAmount("");
+    setInputDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-cost__controls">
+        <div className="new-cost__control">
+          <label>Name</label>
+          <input value={inputName} onChange={nameChangeHandler} type="text" />
+        </div>
+        <div className="new-cost__control">
+          <label>Amount</label>
+          <input
+            value={inputAmount}
+            onChange={amountChangeHandler}
+            type="number"
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-cost__control">
+          <label>Date</label>
+          <input value={inputDate} onChange={dateChangeHandler} type="date" />
+        </div>
+        <div className="new-cost__actions">
+          <button type="submit">Add cost</button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CostForm;
